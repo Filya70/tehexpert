41f81dda940e9f584e5ca7d5dd97b08d407fe532
Filya70 <f0rest70@mail.ru>
2019-04-13 01:48:43 +0700

    готовое адаптивное меню

--- js/js.js
+++ js/js.js
@@ -1,10 +1,72 @@
+
+////Dropdown clild menu
 $('.source').on('mouseover', function () {
     var $src = $(this),
         $srcImg = $src.find('.subtitle-logo').html(),
         $srcTxt = $src.find('.dropdown-child-item').html(),
         $srcSgn = $src.find('.subtitle-text').html();
-    
+
     $('.sub-logo').html($srcImg);
     $('.title').html($srcTxt);
     $('.sub-text').html($srcSgn);
+});
+
+//Burger Menu
+var Menu = {
+
+    el: {
+        ham: $('.menu'),
+        menuTop: $('.menu-top'),
+        menuMiddle: $('.menu-middle'),
+        menuBottom: $('.menu-bottom')
+    },
+
+    init: function () {
+        Menu.bindUIactions();
+    },
+
+    bindUIactions: function () {
+        Menu.el.ham
+            .on(
+                'click',
+                function (event) {
+                    Menu.activateMenu(event);
+                    event.preventDefault();
+                }
+            );
+    },
+
+    activateMenu: function () {
+        Menu.el.menuTop.toggleClass('menu-top-click');
+        Menu.el.menuMiddle.toggleClass('menu-middle-click');
+        Menu.el.menuBottom.toggleClass('menu-bottom-click');
+    }
+};
+
+Menu.init();
+//// Dropdown Menu Burger
+$(document).ready(function () {
+    $('.menu').click(function () {
+        $('.nav-mobile-block').slideToggle(500);
+    });//end slide toggle
+
+    $(window).resize(function () {
+        if ($(window).width() > 300) {
+            $('.nav-mobile-block').removeAttr('style');
+        }
+    });
+});
+//// Dropdown Menu Item and switch color text
+$(document).ready(function () {
+    $('.item').click(function () {
+        $('.dropdown').slideToggle(500);
+        $(this).css('color', 'red');
+    });
+
+
+    $(window).resize(function () {
+        if ($(window).width() > 250) {
+            $('.dropdown', '.item').removeAttr('style');
+        }
+    });
 });
